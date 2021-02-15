9b972da23cb7aaf262ef2749b3e70ab00b1f156b
ravinder sian <rsian761@gmail.com>
2021-02-15 17:21:30 +0000

    add customer dto

---
+++ online-store/src/main/java/com/personal/onlinestore/model/CustomerDTO.java
@@ -0,0 +1,14 @@
+package com.personal.onlinestore.model;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+	
+	private Long customerId;
+	
+	private String firstName;
+	
+	private String lastName;
+
+}
