2c69346b7d8c457aa62a71d5d179591a25e3f1e3
ravinder sian <rsian761@gmail.com>
2021-02-16 16:46:13 +0000

    modify services and tests for dto

--- online-store/src/main/java/com/personal/onlinestore/services/CustomerServiceImpl.java
+++ online-store/src/main/java/com/personal/onlinestore/services/CustomerServiceImpl.java
@@ -2,46 +2,55 @@ package com.personal.onlinestore.services;
 
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.personal.onlinestore.model.Customer;
+import com.personal.onlinestore.model.CustomerDTO;
 import com.personal.onlinestore.repository.CustomerRepository;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
 	
 	private final CustomerRepository repository;
+	
+	private ModelMapper mapper = new ModelMapper();
 
 	public CustomerServiceImpl(CustomerRepository repository) {
 		this.repository = repository;
 	}
 
 	@Override
-	public Customer save(Customer customer) {
-		return repository.save(customer);
-	}
-
-	@Override
-	public void delete(Customer customer) {
-		repository.delete(customer);
+	public CustomerDTO saveAndReturnCustomerDTO(Customer customer) {
+		Customer savedCustomer = repository.save(customer);
+		return mapper.map(savedCustomer, CustomerDTO.class);
 		
 	}
 
 	@Override
-	public Optional<Customer> findById(Long id) {
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+		
+	}
+
+	@Override
+	public Optional<CustomerDTO> findById(Long id) {
 		Optional<Customer> customerOptional = repository.findById(id);
 		if (!customerOptional.isPresent()) {
 			return Optional.empty();
 		}
 		
-		return customerOptional;
+		CustomerDTO customerDTO = mapper.map(customerOptional.get(), CustomerDTO.class);
+		return Optional.of(customerDTO);
 	}
 
 	@Override
-	public Customer updateFirstName(Customer customer, String firstName) {
-		customer.setFirstName(firstName);
-		return repository.save(customer);
+	public CustomerDTO saveCustomerByDTO(Long id, CustomerDTO customerDTO) {
 		
+		Customer customer = mapper.map(customerDTO, Customer.class);
+		customer.setCustomerId(id);
+		
+		return saveAndReturnCustomerDTO(customer);
 	}
 
 }
